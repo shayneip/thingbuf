70f100535fab2ae49dba6bd434b45e5345b74a39
shayneip <shayneardaiz@gmail.com>
2023-05-19 22:40:03 +0000

    Initialized Mayhem

---
+++ fuzz/fuzz_targets/create_buf.rs
@@ -0,0 +1,12 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use thingbuf::ThingBuf;
+
+fuzz_target!(|input: Vec<String>| {
+    if input.len() > 0 {
+        let buf: ThingBuf<String> = ThingBuf::new(input.len());
+        input.iter().for_each(|x| {
+            let _ = buf.push(x.to_string());
+        });
+    }
+});
